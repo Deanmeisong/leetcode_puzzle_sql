5eb23b6241de61f5a47c8f0a28a96eadba202f83
dean <deanmeisong@gmail.com>
2025-04-26 13:02:05 +1000

    Time: 295 ms (95%), Space: 0B (100%) - LeetHub

--- 3366-user-activities-within-time-bounds/3366-user-activities-within-time-bounds.sql
+++ 3366-user-activities-within-time-bounds/3366-user-activities-within-time-bounds.sql
@@ -1,15 +1,13 @@
 # Write your MySQL query statement below
+
+
 with T as (
     select *,
-        rank() over(partition by user_id, session_type order by session_start) as rk
-        from sessions
+    lead(session_start) over (partition by user_id, session_type order by session_start) as next_session_start
+    from sessions
 )
 
-SELECT DISTINCT t1.user_id
-FROM T t1
-JOIN T t2
-  ON t1.user_id = t2.user_id
- AND t1.session_type = t2.session_type
- AND t2.rk = t1.rk + 1  -- t2 is immediately after t1
-WHERE TIMESTAMPDIFF(HOUR, t1.session_end, t2.session_start) <= 12
-ORDER BY t1.user_id;
+select distinct user_id
+    from T
+    where next_session_start is not null 
+    and TIMESTAMPDIFF(hour, session_end, next_session_start) <= 12
